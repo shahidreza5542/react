4b9660d31cc0ad8546d6dbb1300e0ac1dc49f5b6
shahidreza <shahidreza660@gmail.com>
2025-08-22 09:19:05 +0530

    changes

--- 01-reactjs/src/App.jsx
+++ 01-reactjs/src/App.jsx
@@ -3,12 +3,12 @@ import Chai from './Chai'
 
 
 function App() {
+  const username = "+ chai aur code"
   
-
   return ( 
     <>
     <Chai/>
-    <h1>chai aur react</h1>
+    <h1>chai aur react {username}</h1>
     <p>test para</p>
     </>
   )
--- 01-reactjs/src/main.jsx
+++ 01-reactjs/src/main.jsx
@@ -1,10 +1,41 @@
-import { StrictMode } from 'react'
+import React, { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
+function myApp() {
+  return (
+    <div>
+      <h1>custom app</h1>
+    </div>
+  )
+}
+
+
+
+// const ReactElement = {
+//   type: 'a',
+//   props: {
+//     href: 'https://google.com',
+//     target: '_blank'
+//   },
+//   children: 'Click me to visit google'
+// }
+const anotherUser = " chai aur code"
+
+const anotherElement = (
+  <a href="#">visit google</a>
+)
+
+const reactElement = React.createElement(
+  'a',
+  {href: "#", target: "_blank"},
+  'click me to visit google',
+  anotherUser
+)
+
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+
+  <App />
+  
 )
 
